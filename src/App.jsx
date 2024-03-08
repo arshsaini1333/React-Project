e65d23aa02f3c2bdd2cdd301ceab153ff18d7a0c
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-03-08 17:15:11 +0530

    Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Button variant="outlined">Outlined</Button>
+      <Button variant="contained" color="error">
+        Contained
+      </Button>
+      <Button variant="contained" color="success">
+        Contained
+      </Button>
+      <br />
+      <br />
+      <IconButton aria-label="delete" color="primary" variant="contained">
+        <DeleteIcon />
+      </IconButton>
+      <br />
+      <br />
+      <Button variant="contained" startIcon={<DeleteIcon />} color="error">
+        Delete
+      </Button>
     </>
   );
 }
